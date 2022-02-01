46b15285f351451971a49c7ae47d45440d653a47
Logorrheique <rheiquelogor@gmail.com>
2022-02-01 11:33:12 +0100

    base

--- src/App.js
+++ src/App.js
@@ -1,44 +1,37 @@
-import WheatherCard from './Components/wheatherCard/Wheather';
-import './App.css';
-import Header from './Components/Header';
+import WheatherCard from "./Components/wheatherCard/Wheather";
+import "./App.css";
+import Header from "./Components/Header";
 
 const test = {
-    name: 'Mountain View',
-    coords: {
-        lon: -122.08,
-        lat: 37.39
-    },
-    wheather: {
-        main: 'Clear',
-        description: 'clear sky'
-    },
-    main: {
-        temp: 282.55,
-        feels_like: 281.86,
-        temp_min: 280.37,
-        temp_max: 284.26,
-        pressure: 1023,
-        humidity: 100
-    },
-    wind: {
-        speed: 1.5
-    }
+  name: "Mountain View",
+  coords: {
+    lon: -122.08,
+    lat: 37.39,
+  },
+  wheather: {
+    main: "Clear",
+    description: "clear sky",
+  },
+  main: {
+    temp: 282.55,
+    feels_like: 281.86,
+    temp_min: 280.37,
+    temp_max: 284.26,
+    pressure: 1023,
+    humidity: 100,
+  },
+  wind: {
+    speed: 1.5,
+  },
 };
-const header = {
-    test: {
-        lon: 1,
-        lat: 2
-    }
-};
+
 function App() {
-    console.log('heyy');
-    return (
-        <div className="App">
-            <h1>Hellowwww</h1>
-            <Header header={header}></Header>
-            <WheatherCard wheatherObject={test}></WheatherCard>
-        </div>
-    );
+  console.log("heyy");
+  return (
+    <div className="App">
+      <WheatherCard wheatherObject={test}></WheatherCard>
+    </div>
+  );
 }
 
 export default App;
--- src/Components/wheatherCard/Wheather.jsx
+++ src/Components/wheatherCard/Wheather.jsx
@@ -1,27 +1,32 @@
-import Coords from './Coords';
-import WheaterState from './WheatherState';
-import TempMetrics from './TempMetrics';
-import { Wind, Humidity } from './MoreMetrics';
+import Coords from "./Coords";
+import WheaterState from "./WheatherState";
+import TempMetrics from "./TempMetrics";
+import { Wind, Humidity } from "./MoreMetrics";
 
-const WheatherCard = wheatherObject => {
-    return (
-        <div className="wheater">
-            <h1 className="cityName">{wheatherObject.name}</h1>
-            {/* <Coords lon={wheatherObject.coords.lon} lat={wheatherObject.coords.lat}></Coords> */}
-            <WheaterState
-                className="wheatherState"
-                state={wheatherObject.wheather.main}
-                description={wheatherObject.wheather.description}
-            ></WheaterState>
-            <TempMetrics
-                temp={wheatherObject.main.temp}
-                tempFeel={wheatherObject.main.feel_like}
-                tempMin={wheatherObject.main.temp_min}
-                tempMax={wheatherObject.main.temp_max}
-            ></TempMetrics>
-            <Wind windSpeed={wheatherObject.wind.speed}></Wind>
-            <Humidity windSpeed={wheatherObject.main.humidity}></Humidity>
-        </div>
-    );
+const WheatherCard = ({ wheatherObject }) => {
+  return (
+    <>
+      <div className="wheaterCards">
+        <h1 className="cityName">{wheatherObject.name}</h1>
+        <Coords
+          lon={wheatherObject.coords.lon}
+          lat={wheatherObject.coords.lat}
+        ></Coords>
+        <WheaterState
+          className="wheatherState"
+          state={wheatherObject.wheather.main}
+          description={wheatherObject.wheather.description}
+        ></WheaterState>
+        <TempMetrics
+          temp={wheatherObject.main.temp}
+          tempFeel={wheatherObject.main.feel_like}
+          tempMin={wheatherObject.main.temp_min}
+          tempMax={wheatherObject.main.temp_max}
+        ></TempMetrics>
+        <Wind windSpeed={wheatherObject.wind.speed}></Wind>
+        <Humidity windSpeed={wheatherObject.main.humidity}></Humidity>
+      </div>
+    </>
+  );
 };
 export default WheatherCard;
