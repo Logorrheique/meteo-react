e46ce46a8d00399f613a9ffae646ebbbb1c5c901
Logorrheique <rheiquelogor@gmail.com>
2022-02-04 09:42:34 +0100

    start work with many elements

--- src/Components/wheatherCard/Wheather.jsx
+++ src/Components/wheatherCard/Wheather.jsx
@@ -1,37 +1,39 @@
-import BottomContent from './BottomContent/BottomContent';
-import MedianContent from './MedianContent/MedianContent';
-import TopContent from './TopContent/TopContent';
-import './../../style/wheather.css';
+import BottomContent from "./BottomContent/BottomContent";
+import MedianContent from "./MedianContent/MedianContent";
+import TopContent from "./TopContent/TopContent";
+import "./../../style/wheather.css";
 const WheatherCard = ({ objecte }) => {
-    const object = {
-        wheather: 'Clair',
-        temp: 15,
-        tempFeelLike: 18,
-        windspeed: 15,
-        tempMin: -1,
-        tempMax: 2,
-        hum: 20,
-        nameCity: 'Paris'
-    };
-    return (
-        <>
-            <div className="wheatherCard">
-                <img src="http://localhost:3000/img/wheather.png" alt="" />
-                <TopContent
-                    wheather={object.wheather}
-                    wheatherImg={object.wheatherImg}
-                ></TopContent>
-                <MedianContent temp={object.temp} feelTemp={object.tempFeelLike}></MedianContent>
-                <BottomContent
-                    windspeed={object.windspeed}
-                    StrengthImg={object.winImg}
-                    tempMin={object.tempMin}
-                    tempMax={object.tempMax}
-                    hum={object.hum}
-                    cityName={object.nameCity}
-                ></BottomContent>
-            </div>
-        </>
-    );
+  const object = {
+    wheather: "Clair",
+    temp: 15,
+    tempFeelLike: 18,
+    windspeed: 15,
+    tempMin: -1,
+    tempMax: 2,
+    hum: 20,
+    nameCity: "Paris",
+  };
+  return (
+    <>
+      <div className="wheatherCard">
+        <TopContent
+          wheather={object.wheather}
+          wheatherImg={object.wheatherImg}
+        ></TopContent>
+        <MedianContent
+          temp={object.temp}
+          feelTemp={object.tempFeelLike}
+        ></MedianContent>
+        <BottomContent
+          windspeed={object.windspeed}
+          StrengthImg={object.winImg}
+          tempMin={object.tempMin}
+          tempMax={object.tempMax}
+          hum={object.hum}
+          cityName={object.nameCity}
+        ></BottomContent>
+      </div>
+    </>
+  );
 };
 export default WheatherCard;
