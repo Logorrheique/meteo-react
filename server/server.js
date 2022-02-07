7640e6b0f97f379cef59d067aa7d5e330aa32895
Logorrheique <rheiquelogor@gmail.com>
2022-02-07 20:32:21 +0100

    fix

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
-
-app.get("/", (req, res) => {
-  res.set("Content-Type", "text/html");
-  res.send("Hello world !!");
+require('dotenv').config();
+app.get('/', (req, res) => {
+    res.set('Content-Type', 'text/html');
+    res.send('Hello world !!');
 });
 
 app.listen(port, () => {
-  console.log("Server app listening on port " + port);
+    console.log('Server app listening on port ' + port);
 });
