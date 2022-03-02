073d11b11b0ef2f65e74779dbd96a7d045a79e76
Logorrheique <rheiquelogor@gmail.com>
2022-03-02 14:49:22 +0100

    heroku test

--- server/server.js
+++ server/server.js
@@ -1,20 +1,20 @@
 // Depedencies
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT;
 
 //import routes
-const { getWeather } = require("./routes/getWeather");
+const { getWeather, getTest } = require('./routes/getWeather');
 
 //middleware
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
-
-app.use("/api", getWeather);
+app.use('/', getTest);
+app.use('/api', getWeather);
 app.listen(port, () => {
-  console.log("Server app listening on port " + port);
+    console.log('Server app listening on port ' + port);
 });
