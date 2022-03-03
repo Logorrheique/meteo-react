ca29f8474b781d6fcac4d67d5621ca4e33d93df2
Logorrheique <rheiquelogor@gmail.com>
2022-03-03 01:19:30 +0100

    fix 1

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const favicon = require('express-favicon');
 require('dotenv').config();
 const path = require('path');
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT;
 
 const app = express();
 const axios = require('axios');
